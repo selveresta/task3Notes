2197d07b6fb863b80e1bfb78fbdb0e56a144b4e4
selveresta <artem.lypnyk@softsculptor.net>
2023-08-02 22:50:34 +0300

    initial commit with sctucture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from './db/typeorm.module';
+import { NoteModule } from './model/note/note.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule, NoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
